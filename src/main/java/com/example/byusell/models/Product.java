cfcd70a9785654e2e0121299572c197bdbb786e0
Your Name <you@example.com>
2021-11-21 16:56:38 +0300

    Initial commit

---
+++ src/main/java/com/example/byusell/models/Product.java
@@ -0,0 +1,33 @@
+package com.example.byusell.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "author")
+    private String author;
+
+}
