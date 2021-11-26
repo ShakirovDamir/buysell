9bbe32f76b6dced586701d71993b2c3c3c76242f
Your Name <you@example.com>
2021-11-26 15:01:35 +0300

    l_5

---
+++ src/main/java/com/example/byusell/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.example.byusell.controllers;
+
+import com.example.byusell.models.User;
+import com.example.byusell.servises.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/registration")
+    public String createUser(User user){
+        userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl(){
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/byusell/servises/UserService.java
@@ -0,0 +1,30 @@
+package com.example.byusell.servises;
+
+import com.example.byusell.models.User;
+import com.example.byusell.models.enums.Role;
+import com.example.byusell.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean createUser(User user){
+        String email = user.getEmail();
+        if(userRepository.findByEmail(email) != null) return false;
+        user.setActive(true);
+        user.getRoles().add(Role.ROLE_USER);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("Saving new user with email: {}", email);
+        userRepository.save(user);
+        return true;
+    }
+}
